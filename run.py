30b93c791977ef1d9c0ec400afadd6a04832e39b
kageback <kageback@chalmers.se>
2018-05-20 22:42:30 +0200

    rewritten cogsci2018 exp for new version of gridengine

--- run.py
+++ run.py
@@ -1,38 +1,63 @@
 import pickle
-import gridengine.batch as ge
+
+import numpy as np
+
+import gridengine as sge
+from gridengine.queue import Queue, Local
+from gridengine.pipeline import HyperParamSearchExperiment
+
+import evaluate
+import model
+import wcs
 import viz
-import map_reduce
 
 
 def main():
-    num_words_min = 3
-    num_words_max = 11
 
-    args = {}
-    args['script'] = 'model.py'
-    args['max_epochs'] = 10000 #10000
-    args['noise_range'] = [0, 25, 50, 100]
-    args['avg_over'] = 50
-    args['msg_dim_range'] = range(num_words_min, num_words_max+1)
-    job = ge.Job(job_id='avg50')
-    args['job_id'] = job.job_id
 
-    args_path = job.job_dir + '/job_args.pkl'
-    print('saving job arguments as', args_path)
-    with open(args_path, 'wb') as f:
-        pickle.dump(args, f)
+    queue = Local()
+    #queue = Queue(cluster_wd='~/runtime/colorwords/', host='titan.kageback.se', ge_gpu=1, queue_limit=4)
+    #queue = Queue(cluster_wd='~/runtime/colorwords/', host='home.kageback.se', queue_limit=4)
+    #queue = Queue(cluster_wd='~/runtime/colorwords/', host='ttitania.ce.chalmers.se', user='mlusers', queue_limit=4)
 
-    print('Map job')
-    map_reduce.map_job(job, args)
+    queue.sync('.', '.', exclude=['pipelines/*', 'fig/*', 'old/*', 'cogsci/*'], sync_to=sge.SyncTo.REMOTE,
+               recursive=True)
 
-    print('Waiting for tasks to finish...')
-    job.wait()
+    exp = HyperParamSearchExperiment([('avg_over', range(2)),  # 50
+                                     ('noise_range', [0]),  # [0, 25, 50, 100]
+                                     ('msg_dim_range', range(5, 6))],  # range(3,12)
+                                     queue=queue, exp_name='dev')
 
-    print('Reduce job')
-    map_reduce.reduce_job(job)
+    for (params_i, params_v) in exp:
+        print('Param epoch %d of %d' % (params_i[exp.axes['avg_over']], exp.shape[exp.axes['avg_over']]))
+        net = exp.run(model.main,
+                           msg_dim=params_v[exp.axes['msg_dim_range']],
+                           max_epochs=10, #10000
+                           noise_level=params_v[exp.axes['noise_range']],
+                           hidden_dim=20,
+                           batch_size=100,
+                           sender_loss_multiplier=100,
+                           print_interval=1000,
+                           eval_interlval=0)
 
-    #print('plot results')
-    #viz.plot_costs(job)
+        V = exp.run(model.color_graph_V, a=net.result(), cuda=False)
+
+        exp.set_result('gibson_cost', params_i, exp.run(evaluate.compute_gibson_cost, a=net.result()))
+        exp.set_result('regier_cost', params_i, exp.run(wcs.communication_cost_regier, V=V.result()))
+        exp.set_result('wellformedness', params_i, exp.run(wcs.wellformedness, V=V.result()))
+        exp.set_result('combined_criterion', params_i, exp.run(wcs.combined_criterion, V=V.result()))
+        exp.set_result('term_usage', params_i, exp.run(wcs.compute_term_usage, V=V.result()))
+
+
+    print("\nAll tasks queued to clusters")
+
+    # wait for all tasks to complete
+    exp.save()
+    exp.wait(retry_interval=5)
+    queue.sync(exp.pipeline_path, exp.pipeline_path, sync_to=sge.SyncTo.LOCAL, recursive=True)
+
+    print('plot results')
+    viz.plot_costs(exp)
 
 
 if __name__ == "__main__":
