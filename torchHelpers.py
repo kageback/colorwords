391a302f90fc3c8d48bd09f2b75725fa7372eb4e
kageback <kageback@chalmers.se>
2018-06-27 22:50:05 +0200

    use_cuda not set now checks if cuda is available and use if so

--- torchHelpers.py
+++ torchHelpers.py
@@ -2,15 +2,18 @@ import torch
 from torch.autograd import Variable
 
 
-def long_var(tensor, use_cuda=False):
+def long_var(tensor, use_cuda=None):
     return cuda(Variable(torch.LongTensor(tensor)), use_cuda)
 
 
-def float_var(tensor, use_cuda=False):
+def float_var(tensor, use_cuda=None):
     return cuda(Variable(torch.FloatTensor(tensor)), use_cuda)
 
 
-def cuda(var, use_cuda):
+def cuda(var, use_cuda=None):
+    if use_cuda is None:
+        use_cuda = torch.cuda.is_available()
+
     if use_cuda:
         return var.cuda()
     else:
