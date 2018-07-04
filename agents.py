fixed error in softmax agent, updated plotting, added training mode to env

agents.py
@@ -40,9 +40,16 @@ class SoftmaxAgent(BasicAgent):
 
     def forward(self, perception=None, msg=None, tau=1):
 
-        if msg is not None and type(msg.data) == torch.LongTensor:
-            onehot = torch.FloatTensor(len(msg), self.msg_dim)
-            onehot.zero_()
-            msg = Variable(onehot.scatter_(1, msg.data.unsqueeze(1), 1))
+        if msg is not None:
+            # First make discrete input into a onehot distribution (used for eval)
+            if type(msg.data) == torch.LongTensor:
+                onehot = torch.FloatTensor(len(msg), self.msg_dim)
+                onehot.zero_()
+                msg = Variable(onehot.scatter_(1, msg.data.unsqueeze(1), 1))
 
-        return super().forward(perception, msg, tau)
+            h = F.tanh(self.msg_receiver(msg))
+            color_logits = self.color_estimator(h)
+            return F.softmax(color_logits, dim=1)
+
+        else:
+            return super().forward(perception, msg, tau)

com_game.py
@@ -21,6 +21,8 @@ class BaseGame:
         self.batch_size = batch_size
         self.print_interval = print_interval
 
+        self.training_mode = True
+
     def play(self, env, agent_a, agent_b):
         agent_a = th.cuda(agent_a)
         agent_b = th.cuda(agent_b)
@@ -75,15 +77,17 @@ class NoisyChannelContRewardGame(BaseGame):
 
     def communication_channel(self, env, agent_a, agent_b, target, perception):
         # add perceptual noise
-        noise = th.float_var(Normal(torch.zeros(self.batch_size, self.perception_dim),
-                                    torch.ones(self.batch_size, self.perception_dim) * self.perception_noise).sample())
-        perception = perception# + noise
+        if self.training_mode:
+            noise = th.float_var(Normal(torch.zeros(self.batch_size, self.perception_dim),
+                                        torch.ones(self.batch_size, self.perception_dim) * self.perception_noise).sample())
+            perception = perception + noise
         # generate message
         msg_probs = agent_a(perception=perception)
         # add communication noise
-        noise = th.float_var(Normal(torch.zeros(self.batch_size, self.msg_dim),
-                                    torch.ones(self.batch_size, self.msg_dim) * self.com_noise).sample())
-        msg = msg_probs# + noise
+        if self.training_mode:
+            noise = th.float_var(Normal(torch.zeros(self.batch_size, self.msg_dim),
+                                        torch.ones(self.batch_size, self.msg_dim) * self.com_noise).sample())
+            msg = msg_probs + noise
         # interpret message and sample a guess
         guess_probs = agent_b(msg=msg)
         m = Categorical(guess_probs)
@@ -130,9 +134,10 @@ class OneHotChannelContRewardGame(BaseGame):
 
     def communication_channel(self, env, agent_a, agent_b, target, perception):
         # add perceptual noise
-        noise = th.float_var(Normal(torch.zeros(self.batch_size, self.perception_dim),
-                                    torch.ones(self.batch_size, self.perception_dim) * self.perception_noise).sample())
-        perception = perception + noise
+        if self.training_mode:
+            noise = th.float_var(Normal(torch.zeros(self.batch_size, self.perception_dim),
+                                        torch.ones(self.batch_size, self.perception_dim) * self.perception_noise).sample())
+            perception = perception + noise
         # Sample message
         probs = agent_a(perception=perception)
         m = Categorical(probs)

run_noisy_channel.py
@@ -8,36 +8,38 @@ from gridengine.queue import Queue, Local
 import com_enviroments
 import agents
 
-def main():
+def run():
 
 
-    queue = Local()
+    #queue = Local()
     #queue = Queue(cluster_wd='~/runtime/colorwords/', host='titan.kageback.se', ge_gpu=1, queue_limit=4)
     #queue = Queue(cluster_wd='~/runtime/colorwords/', host='home.kageback.se', queue_limit=4)
-    #queue = Queue(cluster_wd='~/runtime/colorwords/', host='ttitania.ce.chalmers.se', user='mlusers', queue_limit=4)
+    queue = Queue(cluster_wd='~/runtime/colorwords/', host='ttitania.ce.chalmers.se', user='mlusers', queue_limit=4)
 
     queue.sync('.', '.', exclude=['pipelines/*', 'fig/*', 'old/*', 'cogsci/*'], sync_to=sge.SyncTo.REMOTE,
                recursive=True)
 
     exp = Experiment(exp_name='noisy_channel',
                      fixed_params=[('env', 'wcs'),
-                                   ('max_epochs', 1000),  #10000
+                                   ('max_epochs', 10000),  #10000
                                    ('hidden_dim', 20),
                                    ('batch_size', 100),
                                    ('perception_dim', 3),
                                    ('target_dim', 330),
-                                   ('perception_noise', 0),
                                    ('print_interval', 1000)],
-                     param_ranges=[('avg_over', range(1)),  # 50
-                                   ('perception_noise', [0, 25]),  # [0, 25, 50, 100],
-                                   ('msg_dim', range(3, 4)), #3, 12
-                                   ('com_noise', [0])], #np.linspace(start=0, stop=1.5, num=1))],
+                     param_ranges=[('avg_over', range(10)),  # 50
+                                   ('perception_noise', [0, 25, 50]),  # [0, 25, 50, 100],
+                                   ('msg_dim', range(9, 12)), #3, 12
+                                   ('com_noise', np.linspace(start=0, stop=0.5, num=20))],
                      queue=queue)
+    queue.sync(exp.pipeline_path, exp.pipeline_path, sync_to=sge.SyncTo.REMOTE, recursive=True)
 
     env = exp.run(com_enviroments.make, exp.fixed_params['env'])
-
+    exp_i = 0
     for (params_i, params_v) in exp:
-        print('Param epoch %d of %d' % (params_i[exp.axes['avg_over']], exp.shape[exp.axes['avg_over']]))
+        print('Scheduled %d experiments out of %d' % (exp_i, len(list(exp))))
+        exp_i += 1
+        #print('Param epoch %d of %d' % (params_i[exp.axes['avg_over']], exp.shape[exp.axes['avg_over']]))
 
         agent_a = agent_b = agents.SoftmaxAgent(msg_dim=params_v[exp.axes['msg_dim']],
                                                 hidden_dim=exp.fixed_params['hidden_dim'],
@@ -47,7 +49,7 @@ def main():
         game = com_game.NoisyChannelContRewardGame(com_noise=params_v[exp.axes['com_noise']],
                                                    msg_dim=params_v[exp.axes['msg_dim']],
                                                    max_epochs=exp.fixed_params['max_epochs'],
-                                                   perception_noise=exp.fixed_params['perception_noise'],
+                                                   perception_noise=params_v[exp.axes['perception_noise']],
                                                    batch_size=exp.fixed_params['batch_size'],
                                                    print_interval=exp.fixed_params['print_interval'])
 
@@ -55,7 +57,7 @@ def main():
 
         V = exp.run(env, call_member='agent_language_map', a=game_outcome.result())
 
-        exp.run(env, call_member='plot_with_colors', V=V.result(), save_to_path='test.png')
+        #exp.run(env, call_member='plot_with_colors', V=V.result(), save_to_path=exp.pipeline_path + 'language_map.png')
 
         exp.set_result('gibson_cost', params_i, exp.run(env, call_member='compute_gibson_cost', a=game_outcome.result()))
         exp.set_result('regier_cost', params_i, exp.run(env, call_member='communication_cost_regier', V=V.result()))
@@ -70,12 +72,28 @@ def main():
     exp.wait(retry_interval=5)
     queue.sync(exp.pipeline_path, exp.pipeline_path, sync_to=sge.SyncTo.LOCAL, recursive=True)
 
+    return exp.pipeline_name
+
+def visualize(pipeline_name):
     print('plot results')
-    viz.plot_com_noise_cost(exp)
-    viz.plot_reiger_gibson(exp)
-    viz.plot_wellformedness(exp)
-    viz.plot_term_usage(exp)
+    exp = Experiment.load(pipeline_name)
+
+    viz.plot_result(exp,
+                    'gibson_cost', 'com_noise', 'msg_dim',
+                    measure_label='Gibson communication efficiency',
+                    x_label='Communication noise',
+                    z_label='terms',
+                    task_result_index=1)
+    viz.plot_result(exp, 'gibson_cost', 'perception_noise', 'msg_dim', task_result_index=1)
+    viz.plot_result(exp, 'gibson_cost', 'com_noise', 'perception_noise', task_result_index=1)
+
+    viz.plot_result(exp, 'regier_cost', 'com_noise', 'msg_dim')
+    viz.plot_result(exp, 'wellformedness', 'com_noise', 'msg_dim')
+    viz.plot_result(exp, 'term_usage', 'com_noise', 'msg_dim')
+    viz.plot_result(exp, 'term_usage', 'perception_noise', 'msg_dim')
+    viz.plot_result(exp, 'term_usage', 'perception_noise', 'com_noise')
 
 
 if __name__ == "__main__":
-    main()
+    pipeline_name = run()
+    visualize(pipeline_name)