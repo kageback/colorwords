f0841427fe43fae4d89b0d1ce18326b93ce53871
kageback <kageback@chalmers.se>
2018-08-28 16:01:30 +0200

    moved the devdatt compareMaps to old/evaluate since it is not currently used

--- old/evaluate.py
+++ old/evaluate.py
@@ -41,3 +41,29 @@ def min_k_cut_cost(V, k, sim):
                 for v2 in C[j]:
                     cost += sim(v1, v2)
     return cost
+
+
+def compareMaps(A, B):
+    wordset_A = {a for a in A.values()}
+    wordset_B = {b for b in B.values()}
+    if not len(wordset_A) == len(wordset_B):
+        return 0
+
+    max_overlap = 0
+    for beta in itertools.permutations(wordset_B):
+        overlap = compareAssignment(A, B, wordset_A, beta)
+        if overlap > max_overlap:
+            max_overlap = overlap
+            best_beta = beta
+
+    return max_overlap
+
+
+def compareAssignment(A, B, word_order_A, word_order_B):
+    scores = []
+    for word_index_a, word_index_b in zip(word_order_A, word_order_B):
+        a_tiles = {t[0] for t in A.items() if t[1] == word_index_a}
+        b_tiles = {t[0] for t in B.items() if t[1] == word_index_b}
+        scores.append(len(b_tiles.intersection(a_tiles)) / max([len(a_tiles), len(b_tiles)]))
+
+    return min(scores)
