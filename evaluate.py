3f5d21e66ef22e2ac43f50126a08668bdb25fc73
kageback <kageback@chalmers.se>
2018-08-28 15:57:23 +0200

    removed deprecated does_noise_matter_for_partitioning_style

--- evaluate.py
+++ evaluate.py
@@ -18,57 +18,6 @@ def compute_cielab_map(wcs, k, iterations=10, bw_boost=1):
     return consensus
 
 
-def does_noise_matter_for_partitioning_style(exp):
-
-    maps = exp.reshape('agent_language_map')
-    terms_used = exp.reshape('term_usage')
-
-    maps_per_noise = exp.reshape('agent_language_map', as_function_of_axes=['perception_noise'])
-    terms_used_per_noise = exp.reshape('term_usage', as_function_of_axes=['perception_noise'])
-
-    asdf = np.array([mean_rand_index(maps[terms_used == t])
-                     for t in np.unique(terms_used)])
-
-    zxcv = np.array([[mean_rand_index(maps_per_noise[noise_i][terms_used_per_noise[noise_i] == t])
-                      for noise_i in range(len(maps_per_noise))]
-                     for t in np.unique(terms_used)])
-    fasd = np.array([a[~np.isnan(a)].mean() for a in zxcv])
-
-    for i in range(len(np.unique(terms_used))):
-        print('{:2d} & {:.3f} & {:.3f} & {:.3f} \\\\ \\hline'.format(np.unique(terms_used)[i],
-                                                                     asdf[i],
-                                                                     fasd[i],
-                                                                     fasd[i] / asdf[i]))
-
-    print('Terms used & Mean rand index for all & Mean rand index within noise group & Ratio \\\\ \\thickhline')
-    number_of_terms = np.unique(terms_used)
-    for t in number_of_terms:
-        # compute average distance between all maps within a number of terms used
-        m = maps[terms_used == t]
-        mean_rand = mean_rand_index(m)
-        # compute average distance between all maps within a number of terms used and perception noise used.
-        v = 0
-        n = 0
-        for noise_i in range(len(maps_per_noise)):
-            m = maps_per_noise[noise_i][terms_used_per_noise[noise_i] == t]
-            a = mean_rand_index(m)
-            if not math.isnan(a):
-                v += a
-                n += 1
-        if n >= 1:
-            mean_per_nois = v / n
-        else:
-            mean_per_nois = float('nan')
-        #print('Terms used = {:2d} | Mean rand index: All = {:.3f} | Noise group = {:.3f} '
-        #      '| Ratio = {:.3f} \\\\'.format(t, mean_rand, mean_per_nois, mean_per_nois / mean_rand))
-
-        # print result as latex table
-        print('{:2d} & {:.3f} & {:.3f} & {:.3f} \\\\ \\hline'.format(t, mean_rand, mean_per_nois, mean_per_nois / mean_rand))
-
-
-
-
-
 def mean_rand_index(ce_a, ce_b=None):
     if ce_b is None:
         skip_trace = 1
